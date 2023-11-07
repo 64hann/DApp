1063d51039476a0b4711158aa9c7234ef22d2824
Han Wei Guang <hanweiguang25@gmail.com>
2023-11-07 21:39:08 +0800

    deploy code

---
+++ nfticket-frontend/hardhat.config.js
@@ -0,0 +1,16 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/ZJxoD3Na-6bb1-ZjWP6A5tuZHmF-W1cu",
+      accounts: 
+        [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
