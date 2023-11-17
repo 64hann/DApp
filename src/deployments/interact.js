e310a46ed294b70f27f93992b0a212c742551472
Han Wei Guang <hanweiguang25@gmail.com>
2023-11-18 00:34:58 +0800

    button calls safeMint success

---
+++ src/deployments/interact.js
@@ -0,0 +1,82 @@
+// require("dotenv").config(
+//   //{ path: "../.env" }
+//   );
+// const API_URL = process.env.API_URL;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const API_URL =
+  "https://eth-sepolia.g.alchemy.com/v2/ZJxoD3Na-6bb1-ZjWP6A5tuZHmF-W1cu";
+const PRIVATE_KEY = "USE YOUR OWN VALUE"
+const CONTRACT_ADDRESS = "0xe7ee2f4695a5bd4d28634255f7a082755f0f39f5";
+const CID = "ipfs://QmYfTFjZ5RCi8fzGEBxudrgNRVsDNN9uTN7dXwZzkYL5E1";
+
+//  import the ABI
+const contract = require("../artifacts/contracts/sample_nft_ticket.sol/Nfticket.json");
+
+// console.log(JSON.stringify(contract.abi));
+
+// create an instance of contract to interact with it
+
+// const { ethers } = require("hardhat");
+const ethers = require("ethers");
+
+//Provider - node provider
+const alchemyProvider = new ethers.providers.JsonRpcProvider(API_URL);
+
+
+// const { ethereum } = window;
+// const provider = new ethers.providers.Web3Provider(ethereum);
+// const signer = provider.getSigner();
+
+//Signer -> eth account that signs the txn
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+//Contract -> deployed contact
+const nft_contract = new ethers.Contract(
+  CONTRACT_ADDRESS,
+  contract.abi,
+  signer
+);
+
+
+  // Call functions inside nftcontract
+  async function main() {
+    // var isMintEnabled = await nft_contract.isMintEnabled()
+    // console.log(`IsMintEnabled: ${isMintEnabled}`);
+    // await nft_contract.toggleMintEnabled()
+    await enableMint();
+    var isMintEnabledAfter = await nft_contract.isMintEnabled();
+    console.log(`IsMintEnabled: ${isMintEnabledAfter}`);
+    await setMaxSupply(100);
+    var supply = await nft_contract.MAX_SUPPLY();
+    console.log(`TotalSupply: ${supply}`);
+    await setMintPrice(500);
+    var mintPrice = await nft_contract.mintPrice();
+    console.log(`MintPrice: ${mintPrice}`);
+
+  };
+
+// main();
+
+async function enableMint() {
+  var isMintEnabled = await nft_contract.isMintEnabled();
+  if (isMintEnabled == false) {
+    const tx = await nft_contract.toggleMintEnabled();
+    await tx.wait();
+  }
+}
+
+async function setMaxSupply(supply) {
+  const tx = await nft_contract.setMaxSupply(supply);
+  await tx.wait();
+}
+
+async function setMintPrice(price) {
+  const tx = await nft_contract.setmintPrice(price);
+  await tx.wait();
+}
+
+async function safeMint(address, uri) {
+  const tx = await nft_contract.safeMint(address, uri)
+  await tx.wait()
+}
--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,12 +1,42 @@
-import { Header } from "../components/Header"
-import { useParams } from "react-router-dom"
-import { Button, Image } from "react-bootstrap"
-import eventsJSON from "../testEvents.json"
+import { Header } from "../components/Header";
+import { useParams } from "react-router-dom";
+import { Button, Image } from "react-bootstrap";
+import eventsJSON from "../testEvents.json";
+
+const ethers = require("ethers");
+const contract = require("../artifacts/contracts/sample_nft_ticket.sol/Nfticket.json");
+const CONTRACT_ADDRESS = "0xe7ee2f4695a5bd4d28634255f7a082755f0f39f5";
+const CID = "ipfs://QmYfTFjZ5RCi8fzGEBxudrgNRVsDNN9uTN7dXwZzkYL5E1";
+const { ethereum } = window;
+const provider = new ethers.providers.Web3Provider(ethereum);
+const signer = provider.getSigner();
+const nft_contract = new ethers.Contract(
+  CONTRACT_ADDRESS,
+  contract.abi,
+  signer
+);
+
+const options = {
+  value: ethers.utils.parseEther("0.0000000000000005"),
+  gasLimit: 500000,
+};
 
 const EventDetails = () => {
-  const params = useParams()
-  const event = eventsJSON.events[params.id]
+  const params = useParams();
+  const event = eventsJSON.events[params.id];
 
+  async function mint() {
+    try {
+      await nft_contract.safeMint(
+        "0x40cea589e15a3cfe981001b048e33dce6bfbbd35", // change this to one obtained from wallet
+        CID,
+        options
+      );
+    } catch (error) {
+      console.error(error);
+      return alert("Transaction failed. Please try again");
+    }
+  }
   return (
     <div>
       <Header />
@@ -36,10 +66,12 @@ const EventDetails = () => {
         >
           {event.description}
         </p>
-        <Button style={{ marginLeft: "10px" }}>BUY TICKETS</Button>
+        <Button style={{ marginLeft: "10px" }} onClick={mint}>
+          BUY TICKETS
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { EventDetails }
+export { EventDetails };
