a5ffc604fb4f6064f7f7b758a61046bb98c104d5
Han Wei Guang <hanweiguang25@gmail.com>
2023-11-18 00:34:58 +0800

    button calls safeMint success

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,27 +1,14 @@
 import { Header } from "../components/Header"
 import { useParams } from "react-router-dom"
 import { Button, Image } from "react-bootstrap"
-// import eventsJSON from "../testEvents.json";
-import { fetchIPFSData } from "../deployments/upload.js"
+// import eventsJSON from "../testEvents.json"
+import { fetchIPFSData } from "../deployments/upload.js";
 
 import { useContext } from "react"
 import { ViewContext } from "../context/ViewProvider"
 
-const eventsJSON = await fetchIPFSData()
+const eventsJSON = await fetchIPFSData();
 
-const ethers = require("ethers")
-const contract = require("../artifacts/contracts/sample_nft_ticket.sol/Nfticket.json")
-const CONTRACT_ADDRESS = "0xe7ee2f4695a5bd4d28634255f7a082755f0f39f5"
-const CID = "ipfs://QmYfTFjZ5RCi8fzGEBxudrgNRVsDNN9uTN7dXwZzkYL5E1"
-const { ethereum } = window
-const provider = new ethers.providers.Web3Provider(ethereum)
-const signer = provider.getSigner()
-const nft_contract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer)
-
-const options = {
-  value: ethers.utils.parseEther("0.0000000000000005"),
-  gasLimit: 500000,
-}
 
 const EventDetails = () => {
   const params = useParams()
@@ -100,7 +87,7 @@ const EventDetails = () => {
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { EventDetails }
+export { EventDetails };
