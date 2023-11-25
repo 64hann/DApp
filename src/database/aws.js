fe54705300d5614733dad001b36be2e033ef8df8
Han Wei Guang <hanweiguang25@gmail.com>
2023-11-25 17:48:15 +0800

    added dynamoDB

---
+++ src/database/aws.js
@@ -0,0 +1,85 @@
+const AWS = require("aws-sdk");
+
+const TABLE_NAME = "forSale";
+
+const GUEST_USER_CREDS = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: "ap-southeast-2:ed87b1b6-1beb-4218-b806-5eb7f74affd3",
+  region: "ap-southeast-2",
+});
+
+const getForSale =  function (req, res) {
+    return new Promise((resolve, reject) => {
+  AWS.config.credentials = GUEST_USER_CREDS;
+  AWS.config.update({ region: "ap-southeast-2" });
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: TABLE_NAME,
+  };
+    docClient.scan(params, async function (err, data) {
+    if (err) {
+      console.log(err);
+      reject(err)
+    } else {
+      const { Items } = data;
+      console.log(Items);
+      resolve(Items);
+
+    }
+  });
+});
+}
+
+const putForSale = async function (req, res) {
+  AWS.config.credentials = GUEST_USER_CREDS;
+  AWS.config.update({ region: "ap-southeast-2" });
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName: TABLE_NAME,
+    Item: { 
+        tokenID: req['title'] + ";" + req['ticketno'],
+        title : req['title'],
+        ticketno : req['ticketno'],
+     },
+  };
+  await docClient.put(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      alert(err);
+    } else {
+      console.log(data);
+      alert("Ticket listed successfully");
+    }
+  });
+};
+
+const removeFromSale = function (req, res) {
+  AWS.config.credentials = GUEST_USER_CREDS;
+  AWS.config.update({ region: "ap-southeast-2" });
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const { id } = req.body;
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id: id,
+    },
+  };
+  docClient.delete(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      res.send({
+        success: false,
+        message: "Error: Server error",
+      });
+    } else {
+      console.log(data);
+      res.send({
+        success: true,
+        message: "Item deleted successfully",
+      });
+    }
+  });
+};
+
+module.exports = { getForSale, putForSale, removeFromSale };
--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -1,7 +1,20 @@
-import { Header } from "../components/Header"
-import { SectionDescription, SectionTitle } from "../components/Titles"
+import { useState, useEffect, useContext } from "react";
+import { Button } from "react-bootstrap";
+import { Header } from "../components/Header";
+import { SectionDescription, SectionTitle } from "../components/Titles";
+import { getForSale } from "../database/aws";
 
 const Marketplace = () => {
+  const [ticketsForSale, setTicketsForSale] = useState([]);
+
+  useEffect(() => {
+    async function fetchTicketsForSale() {
+      const tickets =  await getForSale()
+      setTicketsForSale(tickets);
+      console.log(tickets);
+    }
+    fetchTicketsForSale();
+  },[])
   return (
     <div>
       <Header />
@@ -14,9 +27,12 @@ const Marketplace = () => {
       >
         <SectionTitle text="Marketplace" />
         <SectionDescription text="Explore our user-listed NFT ticket marketplace where each purchase is worry-free, backed by the assurance of blockchain verification." />
+        {ticketsForSale.map((ticket, index) => (
+          <div key={index}>Ticket ID: {(ticket["title"], ticket["ticketno"])}</div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { Marketplace }
+export { Marketplace };
