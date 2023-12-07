da0e849eb22236398cd9cc001e819e7b54afde07
Yogesh <98011898+yogeshshelgaonkar@users.noreply.github.com>
2023-12-07 23:44:58 +0800

    Update unit_test.sol

--- test/unit_test.sol
+++ test/unit_test.sol
@@ -13,39 +13,33 @@ contract testSuite {
     // Runs before each test case
     function beforeEach() public {
         initialOwner = address(this);
-        nfticket = new Nfticket(initialOwner);
-        nfticket.setMaxSupply(100); // Set a max supply for the tests
+        nfticket = new Nfticket(initialOwner, 100, 1 ether); // Set a max supply and mint price for the tests
     }
 
     /// Test if the contract is initially not enabled for minting
     function testInitialMintNotEnabled() public {
-        Assert.equal(nfticket.isMintEnabled(), false, "Minting should initially be disabled");
+        Assert.ok(!nfticket.isMintEnabled(), "Minting should initially be disabled");
     }
 
     /// Test if the contract can toggle the minting status
     function testToggleMint() public {
         bool initialStatus = nfticket.isMintEnabled();
         nfticket.toggleMintEnabled();
-        Assert.notEqual(nfticket.isMintEnabled(), initialStatus, "Minting status should have toggled");
-    }
-
-    /// Test setting the mint price
-    function testSetMintPrice() public {
-        uint256 newPrice = 1 ether;
-        nfticket.setmintPrice(newPrice);
-        Assert.equal(nfticket.mintPrice(), newPrice, "Mint price should be updated");
+        Assert.ok(nfticket.isMintEnabled() != initialStatus, "Minting status should have toggled");
     }
 
     /// Test minting a token with the correct ether value
     function testMintingWithCorrectValue() public {
-        nfticket.setmintPrice(1 ether);
-        nfticket.setMaxSupply(10);
-        nfticket.toggleMintEnabled();
+        // Create a new instance of the contract where the test contract is the owner
+        Nfticket testNfticket = new Nfticket(address(this), 100, 1 ether);
+
+        testNfticket.toggleMintEnabled();
         string memory uri = "http://testuri";
 
         // Attempt to mint with correct ether
-        nfticket.safeMint{value: 1 ether}(address(this), uri, 1);
-        uint256 balance = nfticket.balanceOf(address(this));
-        Assert.equal(balance, 1, "Balance should be 1 after successful mint");
+        testNfticket.safeMint{value: 1 ether}(address(this), uri, 1);
+        uint256 balance = testNfticket.balanceOf(address(this));
+        Assert.ok(balance == 1, "Balance should be 1 after successful mint");
     }
+
 }
